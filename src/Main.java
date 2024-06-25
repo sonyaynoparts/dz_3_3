00e680625a4f9ae7758b695961eb4baa8b34fa78
Sofia Mustetsanu <dzho_1973@mail.ru>
2024-06-26 01:48:41 +0300

    Reformated code

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,18 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
         double amountOfCredit = 1_000_000;
         double interestRateInPercentages = 9.99;
-        int numberOfMonths = 36;
+        int numberOfMonths = 5;
         int monthlyPayment = service.calculate(amountOfCredit, interestRateInPercentages, numberOfMonths);
+        System.out.println("Общий вид:");
         System.out.println(monthlyPayment);
+
+        System.out.println("Для первых входных данных:");
+        System.out.println(service.calculate(1_000_000, 9.99, 12));
+
+        System.out.println("Для вторых входных данных:");
+        System.out.println(service.calculate(1_000_000, 9.99, 24));
+
+        System.out.println("Для третьих входных данных:");
+        System.out.println(service.calculate(1_000_000, 9.99, 36));
     }
 }
